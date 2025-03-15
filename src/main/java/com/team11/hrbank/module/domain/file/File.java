e45a0d25e2930d5f5f7edb9d735f47c770a1c9a2
gunhee <sb04214@naver.com>
2025-03-15 12:48:54 +0900

    "feat: 파일 테이블과 매핑되는 entity 추가#3"

---
+++ src/main/java/com/team11/hrbank/module/domain/file/File.java
@@ -0,0 +1,32 @@
+package com.team11.hrbank.module.domain.file;
+
+import com.team11.hrbank.module.domain.base.BaseEntity; // BaseEntity import
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "files")
+public class File extends BaseEntity {  // BaseEntity 상속받기
+
+    @NotNull
+    @Column(name = "file_name", nullable = false, length = 255)
+    private String fileName;
+
+    @NotNull
+    @Column(name = "format", nullable = false, length = 50)
+    private String format;
+
+    @NotNull
+    @Column(name = "file_path", nullable = false, length = 255)
+    private String filePath;
+
+
+
+    // createdAt, updatedAt은 BaseEntity에서 관리
+}
