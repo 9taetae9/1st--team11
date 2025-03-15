5470c08ef480fd2aa5c5d00f40786594d2927b01
kyungrin Kim <0123jade0123@gmail.com>
2025-03-15 22:06:22 +0900

    feat: EmployeeMapper 구현 - Employee와 EmployeeDto 간 매핑 #1

--- src/main/java/com/team11/hrbank/module/domain/employee/mapper/EmployeeMapper.java
+++ src/main/java/com/team11/hrbank/module/domain/employee/mapper/EmployeeMapper.java
@@ -1,5 +1,17 @@
 package com.team11.hrbank.module.domain.employee.mapper;
 
+import com.team11.hrbank.module.domain.employee.Employee;
+import com.team11.hrbank.module.domain.employee.dto.EmployeeDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
 public interface EmployeeMapper {
+
+  /* TODO 다른 사람들이 Mapper 사용하는 법 확인 필요 */
+  @Mapping(source = "department.id", target = "departmentId")
+  @Mapping(source = "department.name", target = "departmentName")
+  @Mapping(source = "profileImage.id", target = "profileImageId")
+  EmployeeDto toDto(Employee employee);
 
 }
