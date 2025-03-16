5063aa0d0bcef24c507752af88d4170608fb727c
jjhparkk <nomoredkdlel@gmail.com>
2025-03-16 16:40:49 +0900

    refactor: 부서 DepartmentDto 수정 #5

--- src/main/java/com/team11/hrbank/module/domain/department/dto/DepartmentDto.java
+++ src/main/java/com/team11/hrbank/module/domain/department/dto/DepartmentDto.java
@@ -10,7 +10,7 @@ public class DepartmentDto {
   private Long id; //api 명세에 지정되어있음
   private String name;
   private String description;
-  private LocalDate establishDate; //api 명세서에 지정되어있음
+  private LocalDate establishedDate; //api 명세서에 지정되어있음
   private Long employeeCount;
 
 }
