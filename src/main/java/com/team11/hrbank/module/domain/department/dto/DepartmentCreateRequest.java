e90ae2ada91c213b626fb0e9022ba495edb73320
jjhparkk <nomoredkdlel@gmail.com>
2025-03-16 16:42:31 +0900

    feat: 부서 Department CreateRequest, UpdateRequest 구현 #5

--- src/main/java/com/team11/hrbank/module/domain/department/dto/DepartmentCreateRequest.java
+++ src/main/java/com/team11/hrbank/module/domain/department/dto/DepartmentCreateRequest.java
@@ -1,4 +1,23 @@
 package com.team11.hrbank.module.domain.department.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
 public class DepartmentCreateRequest {
+
+  @NotBlank(message = "부서명을 입력해주세요.") //빈칸 입력 불가능함
+  @Length(max = 100, message = "부서명은 100자를 초과할 수 없습니다.")
+  private String name;
+
+  @NotNull(message = "설립일을 입력해주세요.")//빈칸 입력 불가능함
+  private LocalDate establishedDate;
+
+  @NotBlank(message = "부서 설명을 입력해주세요.")//빈칸 입력 불가능함
+  private String description;
 }
