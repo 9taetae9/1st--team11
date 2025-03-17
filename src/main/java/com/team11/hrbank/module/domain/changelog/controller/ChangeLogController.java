8fc7e4162c76243f73a4d830736e3267f4b2fc71
TaeHyoun Kim <kth9903@gmail.com>
2025-03-17 17:02:06 +0900

    feat: 수정 이력 상세조회 및 건수 조회 endpoint 추가 #12

--- src/main/java/com/team11/hrbank/module/domain/changelog/controller/ChangeLogController.java
+++ src/main/java/com/team11/hrbank/module/domain/changelog/controller/ChangeLogController.java
@@ -2,15 +2,18 @@ package com.team11.hrbank.module.domain.changelog.controller;
 
 import com.team11.hrbank.module.domain.changelog.HistoryType;
 import com.team11.hrbank.module.domain.changelog.dto.CursorPageResponseChangeLogDto;
+import com.team11.hrbank.module.domain.changelog.dto.DiffDto;
 import com.team11.hrbank.module.domain.changelog.service.ChangeLogService;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChangeLogController {
 
   private final ChangeLogService changeLogService;
-
 
   @GetMapping//직원 정보 수정 이력 목록 조회. 상세 변경 내용은 포함 x
   public ResponseEntity<CursorPageResponseChangeLogDto> getAllChangeLogs(
@@ -40,10 +42,26 @@ public class ChangeLogController {
 
     InetAddress inetAddress = ipAddress != null ? InetAddress.getByName(ipAddress) : null;
 
-    CursorPageResponseChangeLogDto response = changeLogService.getAllChangeLogs(employeeNumber,
-        type, memo, inetAddress, atFrom, atTo,
-        idAfter, cursor, size, sortField, sortDirection);
+      CursorPageResponseChangeLogDto response = changeLogService.getAllChangeLogs(
+          employeeNumber,
+          type, memo, inetAddress, atFrom, atTo,
+          idAfter, cursor, size, sortField, sortDirection);
 
-    return ResponseEntity.ok(response); //400 잘못된 요청 또는 지원하지 않는 정렬 필드
+      return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/{id}/diffs") // 직원 정보 수정이력 상세 조회
+  public ResponseEntity<List<DiffDto>> getChangeLogDiffs(@PathVariable Long id) {
+      List<DiffDto> diffs = changeLogService.getChangeLogDiffs(id);
+      return ResponseEntity.ok(diffs);
+  }
+
+  @GetMapping("/count") //수정 이력 건수
+  public ResponseEntity<Long> getChangeLogsCount(
+      @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) Instant fromDate,
+      @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) Instant toDate) {
+
+    long changeLogsCount = changeLogService.getChangeLogsCount(fromDate, toDate);
+    return ResponseEntity.ok(changeLogsCount);
   }
 }
