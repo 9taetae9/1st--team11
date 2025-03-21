9716ab8bf06001f0f6194c184d1d9f3db1933e3b
TaeHyoun Kim <kth9903@gmail.com>
2025-03-21 16:45:02 +0900

    refactor: 랜덤 로직 수정 - 고정 73 자리도 랜덤으로 변경

--- src/main/java/com/team11/hrbank/module/domain/EmployeeNumberGenerator.java
+++ src/main/java/com/team11/hrbank/module/domain/EmployeeNumberGenerator.java
@@ -13,22 +13,11 @@ public class EmployeeNumberGenerator {
     int year = LocalDate.now().getYear();
     StringBuilder stringBuilder = new StringBuilder();
 
-    /**
-     * 추신:
-     * min 100_000_000_000 ~ max 999_999_999_999
-     * min + (long)(secureRandom.nexDouble() * (max-min)) 해도 되지만,
-     *
-     * 당장은 아래가 직관적일 것 같아 밑과 같이 작성했습니다.
-     *
-     * 문구는 모두가 어떤 방식으로든 확인 완료하면 삭제합니다.
-     * 더 좋은 방법이 있다면 공유해주세요.
-     **/
-
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 14; i++) {
       stringBuilder.append(secureRandom.nextInt(10));
     }
 
-    return String.format("EMP-%d-73%s", year, stringBuilder);
+    return String.format("EMP-%d-%s", year, stringBuilder);
 
   }
 }
