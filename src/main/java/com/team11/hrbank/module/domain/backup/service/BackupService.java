bf3cfc4813384f9d2442e7f0cf33603356d0b5ab
TaeHyoun Kim <kth9903@gmail.com>
2025-03-22 17:31:40 +0900

    refactor: 백업 정보 없음에 대한 응답을 500에서 200으로 반환하도록 수정

--- src/main/java/com/team11/hrbank/module/domain/backup/service/BackupService.java
+++ src/main/java/com/team11/hrbank/module/domain/backup/service/BackupService.java
@@ -142,7 +142,7 @@ public class BackupService {
         }
 
         return backupHistoryRepository.findTopByStatusOrderByStartAtDesc(status)
-            .orElseThrow(null);
+            .orElse(null);
     }
 
     /**
